c669ed3c52a6c630873d43cc34c38441d0b45ffd
Victor-DS <vdsantiago.1@gmail.com>
2016-03-19 23:50:08 -0300

    Generic Dialog Method.

--- app/src/main/java/app/blog/standard/standardblogapp/model/util/Util.java
+++ app/src/main/java/app/blog/standard/standardblogapp/model/util/Util.java
@@ -1,7 +1,9 @@
 package app.blog.standard.standardblogapp.model.util;
 
+import android.app.AlertDialog;
 import android.app.Application;
 import android.content.Context;
+import android.content.DialogInterface;
 
 /**
  * @author victor
@@ -22,6 +24,16 @@ public class Util extends Application{
 
     public static Context getContext() {
         return application.getApplicationContext();
+    }
+
+    public static void openGenericDialog(Context mContext, int title, int message, DialogInterface.OnClickListener ok) {
+        AlertDialog.Builder adBuilder = new AlertDialog.Builder(mContext);
+        adBuilder.setMessage(message);
+        adBuilder.setTitle(title);
+        adBuilder.setCancelable(true);
+        adBuilder.setPositiveButton(android.R.string.ok, ok);
+        AlertDialog adFinal = adBuilder.create();
+        adFinal.show();
     }
 
 }
