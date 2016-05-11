377d4e3f6dc541b3e6aac8a125037e9316b0da1d
Victor-DS <vdsantiago.1@gmail.com>
2016-05-11 16:34:31 -0300

    First few changes related to background sync.

--- app/src/main/java/app/blog/standard/standardblogapp/controller/service/Syncronizer.java
+++ app/src/main/java/app/blog/standard/standardblogapp/controller/service/Syncronizer.java
@@ -2,13 +2,19 @@ package app.blog.standard.standardblogapp.controller.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
+
+import app.blog.standard.standardblogapp.R;
+import app.blog.standard.standardblogapp.model.util.PublicationHelper;
+import app.blog.standard.standardblogapp.model.util.Util;
 
 public class Syncronizer extends Service {
 
     public Syncronizer() {
     }
 
+    //region Service methods
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -17,13 +23,49 @@ public class Syncronizer extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        //TODO Synchronize here
-
+        new Sync().execute();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
     }
+    //endregion
+
+    //Sync methods
+    private class Sync extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            return PublicationHelper.getInstance(Util.getContext()).sync();
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            Util.dismissNotification();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Util.sendNotification(R.string.app_name, R.string.synchronizing);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            Util.dismissNotification();
+
+            Util.sendNotification(R.string.app_name, R.string.new_posts_synced);
+        }
+
+        @Override
+        protected void onCancelled(Boolean aBoolean) {
+            super.onCancelled(aBoolean);
+            Util.dismissNotification();
+        }
+    }
+
+    //endregion
+
 }
